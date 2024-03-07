25db6065035bc5dad719a76134d5f9cfd4d3c678
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-03-07 22:02:38 +0000

    make final scripts ans implement api background

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const apiUnsplash = "https://source.unsplash.com/1600x900/?";
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
@@ -12,34 +13,35 @@ const windElement = document.querySelector("#wind span");
 
 const weatherContainer = document.querySelector("#weather-data");
 
-
-
 const getWeatherData = async () => {
-
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=metric&lang=pt_br&appid=f45a3ebdec965165d3431688d5a0e736`;
-
-    const res = await fetch(url);
-    const data = await res.json();
-
-    return data;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=metric&lang=pt_br&appid=f45a3ebdec965165d3431688d5a0e736`;
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
 }
 
 const showWeatherData = async () => {
-    const data = await getWeatherData();
+  const data = await getWeatherData();
+  
+  cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp);
+  descElement.innerText = data.weather[0].description;
+  weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+  countryElement.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/64.png`);
+  humidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}km/h`;
 
-    cityElement.innerText = data.name;
-    tempElement.innerText = parseInt(data.main.temp);
-    descElement.innerText = data.weather[0].description;
-    weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
-    countryElement.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/64.png`);
-    humidityElement.innerText = `${data.main.humidity}%`;
-    windElement.innerText = `${data.wind.speed}km/h`;
-
-    weatherContainer.classList.remove("hide");
+  document.body.style.backgroundImage = `url(${apiUnsplash}${data.name.split(' ').join('%20')})`;
+  weatherContainer.classList.remove("hide");
 }
 
 searchBtn.addEventListener("click", (e) => {
-    e.preventDefault()
-
-    showWeatherData();
+  e.preventDefault()
+  showWeatherData();
 })
+
+cityInput.addEventListener("keyup", (e) => {
+  if (e.code === "Enter") {
+    showWeatherData();
+  }
+});
