b56e50014a46ceb4e547bf523b91f1e303656f4a
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-03-07 20:25:26 +0000

    make api call works an change the elements on html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-const apiCountryURL = "https://flagsapi.com//flat/64.png";
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
@@ -14,20 +13,28 @@ const windElement = document.querySelector("#wind span");
 
 const getWeatherData = async () => {
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=metric&appid=f45a3ebdec965165d3431688d5a0e736`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=metric&lang=pt_br&appid=f45a3ebdec965165d3431688d5a0e736`;
 
     const res = await fetch(url);
     const data = await res.json();
 
-    console.log(data);
+    return data;
 }
 
-const showWeatherData = () => {
-    console.log("city");
+const showWeatherData = async () => {
+   const data = await getWeatherData();
+
+   cityElement.innerText = data.name;
+   tempElement.innerText = parseInt(data.main.temp);
+   descElement.innerText = data.weather[0].description;
+   weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+   countryElement.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/64.png`);
+   umidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}km/h`;
 }
 
 searchBtn.addEventListener("click", (e) => {
     e.preventDefault()
 
-    getWeatherData();
+    showWeatherData();
 })
