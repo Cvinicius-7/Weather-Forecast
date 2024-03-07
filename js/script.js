4d74d7ed23c1e9954d7525c1254295b02c684204
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-03-07 19:45:38 +0000

    Add first js scheme

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-//f45a3ebdec965165d3431688d5a0e736
+const apiKey = "f45a3ebdec965165d3431688d5a0e736";
+const apiCountryURL = "https://flagsapi.com//flat/64.png";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const showWeatherData = (city) => {
+    console.log("city");
+}
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    const city = cityInput.value;
+
+    showWeatherData(city);
+})
